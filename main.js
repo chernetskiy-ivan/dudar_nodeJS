78f088efa2ebfa1fcc806657e01af4135a21031a
Ivan <ivanchernetskey@mail.ru>
2021-02-17 17:29:56 +0300

    created server,listen, check url request

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const fs = require('fs')
-
-//асинхронное удаление файла
+// const fs = require('fs')
+//
+// асинхронное удаление файла
 // fs.unlink('./new-one/some_new.txt', () => {
 //     fs.rmdir('new-one', () => {console.log('Папка удалена')})
 // })
@@ -9,13 +9,25 @@ const fs = require('fs')
 //         console.log('Все сработало')
 //     })
 // })
-
-//ВАЖНО!!! ЧТОБЫ УДАЛИТЬ ПАПКУ С ФАЙЛАМИ - СНАЧАЛА НАДА УДАЛИТЬ ФАЙЛЫ А ЗАТЕМ САМУ ПАПКУ
-
-const filereaded = fs.readFile('text.txt', 'utf-8', (err,data) => {
-    console.log(data)
-})
-console.log('test')
+//
+// ВАЖНО!!! ЧТОБЫ УДАЛИТЬ ПАПКУ С ФАЙЛАМИ - СНАЧАЛА НАДА УДАЛИТЬ ФАЙЛЫ А ЗАТЕМ САМУ ПАПКУ
+//
+// const filereaded = fs.readFile('text.txt', 'utf-8', (err,data) => {
+//     console.log(data)
+// })
+// console.log('test')
 // console.log(filereaded)
 //
 // fs.writeFileSync('exemple.txt', 'Привет мир! Встречай меня')
+
+const http = require('http')
+
+const server = http.createServer( (req, res) => {
+    console.log('URL: ' + req.url)
+    res.writeHead(200, {'Content-Type':'text/plain;charset=utf-8'})
+    res.end('Привет мир')
+})
+
+server.listen(3000,'127.0.0.1')
+
+console.log('мы отслеживаем порт 3000')
