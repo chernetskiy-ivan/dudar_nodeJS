2f87821c2b187a38bcbaec534442fa61d936d705
Ivan <ivanchernetskey@mail.ru>
2021-02-17 14:17:29 +0300

    init commit

---
+++ arr.js
@@ -0,0 +1,5 @@
+const arr_len = function(arr) {
+    return 'В массиве ' + arr.length + ' элементов!'
+}
+
+module.exports = arr_len
---
+++ main.js
@@ -0,0 +1,3 @@
+const show = require('./arr')
+
+console.log(show([1,2,34,5,6,7,8]))
