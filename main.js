dadfc34f7b05d46ae2a622b43b6d576ce1cff226
Ivan <ivanchernetskey@mail.ru>
2021-02-17 17:10:23 +0300

    write read files sync/async

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 
 //асинхронное удаление файла
-fs.unlink('./new-one/some_new.txt', () => {
-    fs.rmdir('new-one', () => {console.log('Папка удалена')})
-})
+// fs.unlink('./new-one/some_new.txt', () => {
+//     fs.rmdir('new-one', () => {console.log('Папка удалена')})
+// })
 // fs.mkdir('new-one', () => {
 //     fs.writeFile('./new-one/some_new.txt', 'Привет мир' ,() => {
 //         console.log('Все сработало')
@@ -11,3 +11,11 @@ fs.unlink('./new-one/some_new.txt', () => {
 // })
 
 //ВАЖНО!!! ЧТОБЫ УДАЛИТЬ ПАПКУ С ФАЙЛАМИ - СНАЧАЛА НАДА УДАЛИТЬ ФАЙЛЫ А ЗАТЕМ САМУ ПАПКУ
+
+const filereaded = fs.readFile('text.txt', 'utf-8', (err,data) => {
+    console.log(data)
+})
+console.log('test')
+// console.log(filereaded)
+//
+// fs.writeFileSync('exemple.txt', 'Привет мир! Встречай меня')
